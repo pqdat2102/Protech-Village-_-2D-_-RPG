94338a9355f87985cf9f616881a33eb6b3080c1a
Phạm Quốc Đạt <pqd21022003@gmail.com>
2025-03-04 16:47:21 +0700

    update fade transaction
    update fade transaction 4h47 4/3

--- Assets/Scrips/Area Exit/Area Entrance.cs
+++ Assets/Scrips/Area Exit/Area Entrance.cs
@@ -10,6 +10,8 @@ public class AreaEntrance : MonoBehaviour
         if(transitionName == SceneManagement.Instance.SceneTransitionName)
         {
             PlayerController.Instance.transform.position = this.transform.position;
+            CameraController.Instance.SetPlayerCameraFollow();
+            UIFade.Instance.FadeToClear();
         }
     }
 }
