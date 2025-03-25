14baed2fde1455e69ab3118ab8d3b81a0d6f2550
Phạm Quốc Đạt <pqd21022003@gmail.com>
2025-03-25 18:07:38 +0700

    siêu cấp update

--- Assets/Scrips/Enemy/Enemy Path Fiding.cs
+++ Assets/Scrips/Enemy/Enemy Path Fiding.cs
@@ -29,7 +29,7 @@ public class EnemyPathFiding : MonoBehaviour
         {
             spriteRenderer.flipX = true;
         }
-        else
+        else if(moveDir.x > 0)
         {
             spriteRenderer.flipX = false;
         }
